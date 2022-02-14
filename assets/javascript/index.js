20cfdd7caa7dc062540826d642690b0bef1f8bb6
CodeWithSk <mh18gamming@gmail.com>
2022-02-14 21:19:55 +0530

    resume

---
+++ assets/javascript/index.js
@@ -0,0 +1,21 @@
+function youtube() {
+    window.open(
+        "https://www.youtube.com/channel/UCDPUEDkXtC-ysk2nIDuWAqw", "_blank");
+}
+
+function linkedin() {
+    window.open(
+        "https://www.linkedin.com/in/chetan-koli-a87358220/", "_blank");
+}
+
+function facebook() {
+    window.open(
+        "https://www.facebook.com/sagar.ekfd/", "_blank");
+}
+
+function sendMail()
+{
+    window.open(
+    document.location = "mailto:developer.chetan.koli@gmail.com", "_blank"
+    )
+}
